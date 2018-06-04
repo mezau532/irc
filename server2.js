7d241231fc5f36dfd73e5f90f6e610f24413ae3d
mezau532 <umeza@pdx.edu>
2018-06-04 13:41:42 -0700

    Add files via upload

--- server2.js
+++ server2.js
@@ -11,10 +11,19 @@ const server = net.createServer((connection) => {
     let command = null;
 
     function broadcast( msg ){
-        for( let client in clients ){
-          if( clients[client] !== connection ){
-            clients[client].write(msg);
-          }
+        if (room === null) {
+            connection.write('Please join a room first: [JOIN ROOM: <room>]\r\n');
+            return;
+        }
+        for( let r in rooms ){
+            if( r === room ){
+                for ( let client in rooms[r] ){
+                    if(rooms[r][client] != connection){
+                        rooms[r][client].write(msg);
+                    } 
+                }
+
+            }
         }
     }
 
@@ -30,9 +39,15 @@ const server = net.createServer((connection) => {
         if (clientName == null) {
             if (command.includes('POST USERNAME: ')) {
                 clientName = command.replace('POST USERNAME: ', '');
+                if (Object.keys(clients).includes(clientName)) {
+                    connection.write(`${clientName} is taken please choose another name\r\n`);
+                    clientName = null;
+                    command = null;
+                    return;
+                }
                 clients[clientName] = connection;
                 connection.write(` - Welcome! , There are ${clientCount} active users\r\n`);
-                broadcast(` - ${clientName} has joined the room\r\n`);
+              //  broadcast(` - ${clientName} has joined the room\r\n`);
                 command = null;
             }
             else {
@@ -44,6 +59,9 @@ const server = net.createServer((connection) => {
                 delete clients[clientName];
                 clientName = command.replace('PUT USERNAME: ', '');
                 clients[clientName] = connection;
+                if (room != null && rooms[room] != null) {
+                    delete rooms[room][clientName];
+                }
                 connection.write(`Username changed to ${clientName}\r\n`);
                 command = null;
             } else if (command.includes('POST ROOM: ')) {
@@ -51,11 +69,43 @@ const server = net.createServer((connection) => {
                 rooms[room] = [];
                 connection.write(`Created room ${room}\r\n`);
                 command = null;
+                room = null;
             } else if (command.includes('GET ROOMS')) {
                 connection.write(`${Object.keys(rooms)}\r\n`);
                 command = null;
+            } else if (command.includes('JOIN ROOM: ')) {
+                room = command.replace('JOIN ROOM: ', '');
+                var roomsList = Object.keys(rooms);
+                if (!roomsList.includes(room)) {
+                    connection.write(`no room ${room}\r\n`);
+                } else {
+                    rooms[room][clientName] = connection;
+                    var roomClientsCount = Object.keys(rooms[room]).length;
+                    connection.write(` - Welcome to ${room}! , There are ${roomClientsCount} users in this room\r\n`);
+                    broadcast(`${clientName} joined the room\r\n`);
+                }
+            } else if (command.includes('LEAVE ROOM')) {
+                if (room != null && rooms[room] != null) {
+                    delete rooms[room][clientName];
+                    broadcast(`${clientName} left the room\r\n`);
+                    room = null;
+                }
+            } else if (command.includes('POST MESSAGE: ')) {
+                var msg = command.replace('POST MESSAGE: ', '');
+                var time = new Date(Date.now());
+                broadcast(`[${time}] ${clientName} - ${msg}\r\n`);
+            } else if (command.includes('GET USERS: ')){
+                var checkRoom = command.replace('GET USERS: ', '');
+                if (rooms[checkRoom] != null) {
+                    connection.write(`${Object.keys(rooms[checkRoom])}\r\n`);
+                    command = null;
+                } else {
+                    connection.write(`${checkRoom} room does not exist`);
+                }
+            } else {
+                connection.write('invalid command\r\n');
+                command = null;
             }
-            broadcast(`${clientName} - ${command}\r\n`);
         }
         
     });
@@ -65,6 +115,9 @@ const server = net.createServer((connection) => {
         console.log('client disconnected');
         broadcast(`${clientName} disconnected\r\n`);
         delete clients[clientName];
+        if (room != null && rooms[room != null]) {
+            delete rooms[room][clientName];
+        }
         console.log(`${clientCount} clients connected`);
     });
 
