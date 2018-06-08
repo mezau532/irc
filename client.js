cefdc1c792949b559adad2b16fbbe5690bb3107c
Ulysses Meza <umeza@pdx.edu>
2018-06-07 17:44:24 -0700

    Revert "use 2 spaces like a regular human being"
    This reverts commit 94f1f0ffe32f2de2a460e889962c9037d8928bd2.

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 var net = require('net');
 
 var client = net.connect({
-  host: 'ec2-18-221-58-190.us-east-2.compute.amazonaws.com',
-  port: '4000',
+    host: 'ec2-18-221-58-190.us-east-2.compute.amazonaws.com',
+    port: '4000',
 });
 
 client.pipe(process.stdout);
